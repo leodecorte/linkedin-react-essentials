09a6e2d5f2592cc7914b3a653faa9e57253c0d69
leodecorte <leodecorte@hotmail.com>
2021-09-13 23:26:43 +0200

    introduction-react-testing-library

---
+++ react-essentials/Exes/6.React_Testing/6.3.Introducing_react_testing_library/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+  
+  return (
+    <div>
+      <h1>Hello React Testing Library</h1>
+    </div>
+  );
+}
+
+
+
+
+export default App;
