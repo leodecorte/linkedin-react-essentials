87710b837938aebaba97081c3616d7fc9a4ae3b0
leodecorte <leodecorte@hotmail.com>
2021-09-14 22:46:03 +0200

    testing-hooks-with-react-testing-libray

---
+++ react-essentials/src/Checkbox.test.js
@@ -0,0 +1,10 @@
+import { fireEvent } from "@testing-library/dom";
+import { render } from "@testing-library/react";
+import { Checkbox } from "./Checkbox";
+
+test("Selecting checkbox", () => {
+    const { getByLabelText} = render(<Checkbox />);
+    const checkbox = getByLabelText(/not checked/);
+    fireEvent.click(checkbox);
+    expect(checkbox.checked).toEqual(true);
+})
